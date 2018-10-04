8b50209c9860f201e0b3ba6fb6e06f82cdababb8
srishtee jain <32458619+srishjain@users.noreply.github.com>
2018-10-04 20:48:28 +0530

    code for arduino controlled car

---
+++ sketch_jun23a.ino
@@ -0,0 +1,81 @@
+char data = 0;                //Variable for storing received data
+const int motorA1=13;
+const int motorA2=12;
+const int motorB1=9;
+const int motorB2=10;
+void left();
+void right();
+void forward();
+void backward();
+void stop1();
+void setup() 
+{
+  Serial.begin(9600);         //Sets the data rate in bits per second (baud) for serial data transmission
+  pinMode(motorA1, OUTPUT);
+  pinMode(motorA2,OUTPUT);
+  pinMode(motorB1,OUTPUT);
+  pinMode(motorB2,OUTPUT);
+}
+
+void loop()
+{
+  if(Serial.available() > 0)  // Send data only when you receive data:
+  {
+    data = Serial.read();      //Read the incoming data and store it into variable data
+    Serial.print(data);        //Print Value inside data in Serial monitor
+    Serial.print("\n");        //New line 
+   if(data == '1')            //Checks whether value of data is equal to 1 
+      forward();  //If value is 1 then LED turns ON
+   else if(data == '2')       //Checks whether value of data is equal to 0
+      backward();
+   else if (data=='3')
+      left();
+   else if(data=='4')
+      right();
+   else if(data=='5')
+     stop1();
+      //If value is 0 then LED turns OFF
+  }                            
+ 
+}
+
+void left()
+{
+  digitalWrite(motorA1,HIGH);
+  digitalWrite(motorA2,LOW);
+  digitalWrite(motorB1,LOW);
+  digitalWrite(motorB2,LOW);
+}
+
+void right()
+{
+  digitalWrite(motorA1,LOW);
+  digitalWrite(motorA2,LOW);
+  digitalWrite(motorB1,HIGH);
+  digitalWrite(motorB2,LOW);
+}
+
+void forward()
+{
+  digitalWrite(motorA1,HIGH);
+  digitalWrite(motorA2,LOW);
+  digitalWrite(motorB1,HIGH);
+  digitalWrite(motorB2,LOW);
+}
+
+void backward()
+{
+  digitalWrite(motorA1,LOW);
+  digitalWrite(motorA2,HIGH);
+  digitalWrite(motorB1,LOW);
+  digitalWrite(motorB2,HIGH);
+}
+void stop1()
+{
+  digitalWrite(motorA1,LOW);
+  digitalWrite(motorA2,LOW);
+  digitalWrite(motorB1,LOW);
+  digitalWrite(motorB2,LOW);
+}
+
+
